f84bf7eaa29f151bd42b14a0a1837ffdd918452e
SanttuF <santtu.f@gmail.com>
2024-05-05 04:10:24 +0300

    new test

--- game-of-life/app_test.py
+++ game-of-life/app_test.py
@@ -3,3 +3,8 @@ from app import Board
 def test_Board_exists():
   board = Board()
   assert board is not None
+
+def test_Board_has_size():
+  board = Board(1, 2)
+  assert board.x == 1
+  assert board.y == 2
