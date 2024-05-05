e728c7956fbba4afae8cdae7eacd26fc9c408a3e
SanttuF <santtu.f@gmail.com>
2024-05-05 04:18:13 +0300

    test passed

--- game-of-life/app.py
+++ game-of-life/app.py
@@ -4,6 +4,7 @@ class Board:
   def __init__(self, x=0, y=0):
     self.x = x
     self.y = y
+    self.board = [['.' for _ in range(x)] for _ in range(y)]
 
 if __name__ == "__main__":
   print(sys.argv[1])
--- game-of-life/app_test.py
+++ game-of-life/app_test.py
@@ -10,5 +10,7 @@ def test_Board_has_size():
   assert board.y == 2
 
 def test_Board_has_board():
-  board = Board(1, 2)
+  board = Board(4, 5)
   assert board.board is not None
+  assert len(board.board) == 5
+  assert len(board.board[0]) == 4
