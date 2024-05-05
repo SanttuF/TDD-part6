bca6f7aff80d43315b4164cffa3becaf02dde7a8
SanttuF <santtu.f@gmail.com>
2024-05-05 04:21:24 +0300

    refactor

--- game-of-life/app_test.py
+++ game-of-life/app_test.py
@@ -1,16 +1,17 @@
 from app import Board
 
-def test_Board_exists():
-  board = Board()
-  assert board is not None
+class TestBoard:
+  def test_Board_exists(self):
+    board = Board()
+    assert board is not None
 
-def test_Board_has_size():
-  board = Board(1, 2)
-  assert board.x == 1
-  assert board.y == 2
+  def test_Board_has_size(self):
+    board = Board(1, 2)
+    assert board.x == 1
+    assert board.y == 2
 
-def test_Board_has_board():
-  board = Board(4, 5)
-  assert board.board is not None
-  assert len(board.board) == 5
-  assert len(board.board[0]) == 4
+  def test_Board_has_board(self):
+    board = Board(4, 5)
+    assert board.board is not None
+    assert len(board.board) == 5
+    assert len(board.board[0]) == 4
