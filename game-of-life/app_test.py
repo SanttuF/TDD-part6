d2fec4580d4df5528ed9961166f2bc71e7daccc6
SanttuF <santtu.f@gmail.com>
2024-05-10 01:53:12 +0300

    updated tests

--- game-of-life/app_test.py
+++ game-of-life/app_test.py
@@ -114,22 +114,22 @@ class TestEndoding:
   def test_Simple_Encode(self):
     string = 'oo'
     encoded = RLEFileHandler.encode(string)
-    assert encoded == '2o'
+    assert encoded == '2o!'
   
   def test_Singular_Encode(self):
     string = 'o'
     encoded = RLEFileHandler.encode(string)
-    assert encoded == 'o'
+    assert encoded == 'o!'
 
   def test_multiple_letters_Encode(self):
     string = 'ooooboo'
     encoded = RLEFileHandler.encode(string)
-    assert encoded == '4ob2o'
+    assert encoded == '4ob2o!'
 
   def test_linechange_Encode(self):
     string = 'oo$bb'
     encoded = RLEFileHandler.encode(string)
-    assert encoded == '2o$2b'
+    assert encoded == '2o$2b!'
 
 
 class TestRLEReader:
